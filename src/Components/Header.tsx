a0daa2fa9bf0d9851759fc13f7a5c3fa3ac3dc2e
Luka Badali <150358418+Bdmnss@users.noreply.github.com>
2025-05-11 21:36:00 +0400

    Add overlay

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 
 const Header = () => {
   const [activeTab, setActiveTab] = useState("ALL");
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
   const tabs = ["ALL", "CLOTHES", "TECH"];
 
   return (
-    <header className="fixed top-0 z-30 flex w-full items-center justify-between bg-white px-72 pt-8">
+    <header
+      className="fixed top-0 z-30 flex w-full items-center justify-between bg-white px-72 pt-8"
+      onClick={() => {
+        setIsOverlayOpen(false);
+      }}
+    >
       <div className="flex gap-6">
         {tabs.map((tab) => {
           return (
@@ -22,7 +28,24 @@ const Header = () => {
 
       <img src="/brandIcon.svg" alt="Brand Icon" />
 
-      <img src="/cartIcon.svg" alt="Cart Icon" />
+      <img
+        src="/cartIcon.svg"
+        alt="Cart Icon"
+        className="cursor-pointer"
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsOverlayOpen(!isOverlayOpen);
+        }}
+      />
+
+      {isOverlayOpen && (
+        <div
+          className="bg-overlay absolute left-0 top-20 z-20 flex h-screen w-full items-center justify-center"
+          onClick={() => {
+            setIsOverlayOpen(false);
+          }}
+        ></div>
+      )}
     </header>
   );
 };
